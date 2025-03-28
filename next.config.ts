4a9fe597b535aac67e376e95baba23550dc7e5af
Benoit Hiller <benoit.hiller@gmail.com>
2025-03-28 17:09:15 -0400

    enable production source maps
    The source is public anyway so I may as well make it easier to inspect
    my own code

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
+  productionBrowserSourceMaps: true,
   output: 'export',
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   env: {
