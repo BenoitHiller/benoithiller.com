f3e3a42a6b0f10677ee7bd67d490787fd0b7d819
Benoit Hiller <benoit.hiller@gmail.com>
2025-03-28 18:37:24 -0400

    set fixed print viewport

--- src/app/(print)/layout.tsx
+++ src/app/(print)/layout.tsx
@@ -1,4 +1,12 @@
 import './print.css';
+import type { Viewport } from 'next';
+
+export const viewport: Viewport = {
+  // 8.5 inches at the CSS standard of 96dpi
+  width: `${8.5 * 96}px`,
+  // Show the whole thing zoomed out
+  initialScale: undefined
+};
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <>{children}</>;
