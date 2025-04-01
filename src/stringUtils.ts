61f3d8acd0d7118c6edff1d894e52eef926739a1
Benoit Hiller <benoit.hiller@gmail.com>
2025-03-31 20:54:14 -0400

    simplify joined template string
    This was trying to do too much before. The actual usage I have so far is
    very simple string collapsing. Other variants can be added only as
    needed.

--- src/stringUtils.ts
+++ src/stringUtils.ts
@@ -1,12 +1,14 @@
+/**
+ * Join a template string onto a single line such that newlines, and any
+ * whitespace immediately surrounding a that newline, is collapsed into a
+ * single space.
+ */
 const joined: typeof String.raw = (template, ...expressions) => {
   let string = String.raw(template, ...expressions);
 
-  string = string.replace(/^[ \t]+/m, '');
-  const initialWhitespace = string.match(/^\s+/m);
-  if (initialWhitespace) {
-    const whitespaceRegex = new RegExp(initialWhitespace[0], 'gm');
-    string = string.replaceAll(whitespaceRegex, ' ');
-  }
+  string = string.replace(/^\s+/m, '');
+  string = string.replace(/\s+$/, '');
+  string = string.replaceAll(/\n\s*/gm, ' ');
 
   return string;
 };
