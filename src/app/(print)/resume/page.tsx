81e6f6057ea6e629537b65e887b247525b6dd539
Benoit Hiller <benoit.hiller@gmail.com>
2025-07-06 14:04:40 -0400

    Restructure resume to be parseable
    Many tools are not smart enough to handle an unlabelled experience
    section correctly, even when they are capable of deducing that the
    unlabelled content is obviously job experience because this is a resume.
    
    This results in things like trying to read the first company name as a
    section header and leaving everything misaligned by one row.

--- src/app/(print)/resume/page.tsx
+++ src/app/(print)/resume/page.tsx
@@ -25,15 +25,15 @@ const Job: React.FC<{
   to: string;
   children?: React.ReactNode;
 }> = ({ title, company, from, to, children }) => (
-  <div className="flex gap-4 resume-section">
-    <div className="resume-column-1">
-      <div className="header">{company}</div>
+  <div className="flex flex-col">
+    <div className="flex gap-2">
+      <div className="font-semibold text-gray-800">{company}</div>
       <div>{title}</div>
       <div className="font-light">
         {from}&ndash;{to}
       </div>
     </div>
-    <ul className="w-full shrink light-text">{children}</ul>
+    <ul className="mt-1 ml-[1em] shrink light-text">{children}</ul>
   </div>
 );
 
@@ -42,18 +42,26 @@ const skillSort = R.sort<string>(R.ascend(normalizeSkill));
 const buildSkillsList = R.pipe(skillSort, R.intersperse(', '), R.join(''));
 
 const SkillsSection: React.FC<{ title: string; skills: string[] }> = ({ title, skills }) => (
-  <div className="resume-section flex gap-4">
-    <div className="resume-column-1">{title}</div>
+  <div className="resume-section">
+    <SectionHeader>{title}</SectionHeader>
 
     <ul className="shrink light-text">{buildSkillsList(skills)}</ul>
   </div>
+);
+
+const SectionHeader: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <h2 className="text-sm">{children}</h2>
+);
+
+const Title: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <h1 className="resume-column-1 text-lg font-semibold text-gray-800">{children}</h1>
 );
 
 function Page() {
   return (
     <article className="text-sm text-gray-700">
       <div className="resume-section flex gap-4 items-baseline">
-        <h1 className="resume-column-1">Benoit Hiller</h1>
+        <Title>Benoit Hiller</Title>
         <div className="flex items-baseline gap-8 w-full shrink">
           <HeaderLink
             label="email:"
@@ -68,78 +76,86 @@ function Page() {
           />
         </div>
       </div>
-      <Job
-        title="Senior Software Engineer"
-        company="Hockeystick.co inc."
-        from="Sept 2017"
-        to="Apr 2022"
-      >
-        <Task>
-          Led the frontend development team on multiple flagship products using JavaScript,
-          TypeScript, React, Next.js, Gatsby, and GraphQL.
-        </Task>
-        <Task>
-          Simultaneously contributed to backend development for those products in C# and Elixir, as
-          well as to infrastructure design and implementation on AWS and Azure.
-        </Task>
-        <Task>
-          Designed and implemented a shared authentication service for Hockeystick's applications in
-          Ruby on Rails, providing a GraphQL API for user management as well as OIDC and SAML IdP
-          integration.
-        </Task>
-        <Task>
-          Championed numerous engineering processes improvements including:
-          <ul className="ml-[1em]">
-            <Task>Developing code style guides.</Task>
-            <Task>Implementing PR checks and CI build infrastructure.</Task>
-            <Task>
-              Building custom Jira and Slack integrations to ease coordination with the rest of the
-              team.
-            </Task>
-            <Task>
-              Identifying developer pain points and building out tooling or code solutions to
-              address them.
-            </Task>
-            <Task>
-              Maintaining the corresponding editor configurations and documentation for all of the
-              aforementioned.
-            </Task>
-          </ul>
-        </Task>
-        <Task>
-          Mentored junior team members, co-op students, and even a few outside contractors in
-          software development.
-        </Task>
-        <Task>
-          Coordinated directly with clients on technical specifications for custom features,
-          external API integrations, and integrations with external authentication providers.
-        </Task>
-        <Task>
-          Took over full platform ownership during the interim when the company was looking for a
-          new CTO and performed the technical interviews for that position.
-        </Task>
-      </Job>
-      <Job title="Software Engineer" company="Hockeystick.co inc." from="Jan 2017" to="Sep 2017">
-        <Task>
-          Served as the primary code contributor on Hockeystick's Reporting and Dealflow
-          applications in Ruby on Rails.
-        </Task>
-        <Task>
-          Designed and built a React GUI for parsing income statements and balance sheets into a
-          standard format so as to aggregate data between differing company reporting practices.
-        </Task>
-      </Job>
-      <Job title="Software Developer" company="Imation (now Nexsan)" from="May 2013" to="Jul 2014">
-        <Task>
-          Worked as a fullstack developer on the configuration UI for Imation's NAS products using
-          ActionScript and Java.
-        </Task>
-        <Task>Implemented libzfs JNA bindings for configuring ZFS from Java.</Task>
-        <Task>
-          Wrote software to apply, verify, and rollback network setting changes via the
-          configuration UI without interrupting the connection to the user.
-        </Task>
-      </Job>
+      <SectionHeader>Experience</SectionHeader>
+      <div className="flex flex-col gap-2 section-border-bottom">
+        <Job
+          title="Senior Software Engineer"
+          company="Hockeystick.co inc."
+          from="Sept 2017"
+          to="Apr 2022"
+        >
+          <Task>
+            Led the frontend development team on multiple flagship products using JavaScript,
+            TypeScript, React, Next.js, Gatsby, and GraphQL.
+          </Task>
+          <Task>
+            Simultaneously contributed to backend development for those products in C# and Elixir,
+            as well as to infrastructure design and implementation on AWS and Azure.
+          </Task>
+          <Task>
+            Designed and implemented a shared authentication service for Hockeystick's applications
+            in Ruby on Rails, providing a GraphQL API for user management as well as OIDC and SAML
+            IdP integration.
+          </Task>
+          <Task>
+            Championed numerous engineering processes improvements including:
+            <ul className="ml-[1em]">
+              <Task>Developing code style guides.</Task>
+              <Task>Implementing PR checks and CI build infrastructure.</Task>
+              <Task>
+                Building custom Jira and Slack integrations to ease coordination with the rest of
+                the team.
+              </Task>
+              <Task>
+                Identifying developer pain points and building out tooling or code solutions to
+                address them.
+              </Task>
+              <Task>
+                Maintaining the corresponding editor configurations and documentation for all of the
+                aforementioned.
+              </Task>
+            </ul>
+          </Task>
+          <Task>
+            Mentored junior team members, co-op students, and even a few outside contractors in
+            software development.
+          </Task>
+          <Task>
+            Coordinated directly with clients on technical specifications for custom features,
+            external API integrations, and integrations with external authentication providers.
+          </Task>
+          <Task>
+            Took over full platform ownership during the interim when the company was looking for a
+            new CTO and performed the technical interviews for that position.
+          </Task>
+        </Job>
+        <Job title="Software Engineer" company="Hockeystick.co inc." from="Jan 2017" to="Sep 2017">
+          <Task>
+            Served as the primary code contributor on Hockeystick's Reporting and Dealflow
+            applications in Ruby on Rails.
+          </Task>
+          <Task>
+            Designed and built a React GUI for parsing income statements and balance sheets into a
+            standard format so as to aggregate data between differing company reporting practices.
+          </Task>
+        </Job>
+        <Job
+          title="Software Developer"
+          company="Imation (now Nexsan)"
+          from="May 2013"
+          to="Jul 2014"
+        >
+          <Task>
+            Worked as a fullstack developer on the configuration UI for Imation's NAS products using
+            ActionScript and Java.
+          </Task>
+          <Task>Implemented libzfs JNA bindings for configuring ZFS from Java.</Task>
+          <Task>
+            Wrote software to apply, verify, and rollback network setting changes via the
+            configuration UI without interrupting the connection to the user.
+          </Task>
+        </Job>
+      </div>
       <SkillsSection
         title="Programming Languages"
         skills={[
@@ -195,8 +211,8 @@ function Page() {
           'Vagrant'
         ]}
       />
-      <div className="resume-section flex gap-4">
-        <div className="resume-column-1">Education</div>
+      <div className="resume-section">
+        <SectionHeader>Education</SectionHeader>
 
         <div className="shrink light-text">
           Bachelor of Science: Computer Science from McGill University (2013)
