f712da23161011d2dda748a6df25a9f5a11399c2
Benoit Hiller <benoit.hiller@gmail.com>
2026-05-19 16:53:53 -0400

    Get rid of route groups
    The () are terrible and I'm only using it for one page so I reorganized things a little bit to make
    it less frustrating to navigate the repository.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,21 @@
 import type { Metadata } from 'next';
-import { M_PLUS_2, Cascadia_Code } from 'next/font/google';
-import FontFallbackDefinition from '@/components/FontFallbackDefinition';
-import './globals.css';
+import urlHelper from '@/urlHelper';
+import '@/app/globals.css';
 import { metadataBase } from '@/sharedMetadata';
 import ClientContext from '@/components/ClientContext';
+import Link from 'next/link';
+import { M_PLUS_2, Cascadia_Code } from 'next/font/google';
+
+export const metadata: Metadata = {
+  title: 'Benoit Hiller',
+  description: 'The website of Benoit Hiller.',
+  alternates: {
+    types: {
+      'application/rss+xml': urlHelper('/blog/rss.xml')
+    }
+  },
+  metadataBase
+};
 
 // TODO: validate hinting is present
 const mPlus = M_PLUS_2({
@@ -18,20 +30,35 @@ const cascadia = Cascadia_Code({
   fallback: ['monospace']
 });
 
-export const metadata: Metadata = {
-  title: 'Benoit Hiller',
-  description: 'The website of Benoit Hiller.',
-  metadataBase
-};
+function HeaderNavLink({ href, children }: React.PropsWithChildren<{ href: string }>) {
+  return (
+    <Link className="text-md/6 text-gray-950" href={href}>
+      {children}
+    </Link>
+  );
+}
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function Layout({
+  children,
+  print
+}: Readonly<{ children: React.ReactNode; print: React.ReactNode }>) {
   return (
     <ClientContext>
       <html lang="en" className={`scroll-pt-18 ${mPlus.variable} ${cascadia.variable}`}>
-        <head>
-          <FontFallbackDefinition />
-        </head>
-        <body className="bg-body-background">{children}</body>
+        <body className="bg-body-background">
+          {print}
+          <header className="non-printable fixed inset-x-0 z-10 bg-white border-b border-gray-950/5">
+            <div className="container mx-auto">
+              <nav className="mx-6 flex h-14 gap-4 items-center">
+                <HeaderNavLink href="/">Home</HeaderNavLink>
+                <HeaderNavLink href="/blog">Blog</HeaderNavLink>
+              </nav>
+            </div>
+          </header>
+          <section className="non-printable pt-14 min-h-dvh container mx-auto max-lg:w-min">
+            <div className="max-xl:max-w-dvw">{children}</div>
+          </section>
+        </body>
       </html>
     </ClientContext>
   );
